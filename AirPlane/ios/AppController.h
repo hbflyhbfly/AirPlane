9244985e2330b99a6f793fe8a005b40e615ce1ad
syuuhi <hbflyhbfly@gmail.com>
2014-03-10 13:57:50 +0800

    Initial Commit

---
+++ AirPlane/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  AirPlaneAppController.h
+//  AirPlane
+//
+//  Created by Syuuhi on 14-3-10.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ AirPlane/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  AirPlaneAppController.h
+//  AirPlane
+//
+//  Created by Syuuhi on 14-3-10.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
